48642dd17da6c55800a39492cf466fd699e32be7
CarL021D <tcarl771@gmail.com>
2024-08-02 00:14:12 +0100

    non bloquant added: select

--- srcs/ft_traceroute.c
+++ srcs/ft_traceroute.c
@@ -30,36 +30,73 @@ void print_traceroute_first_output(t_data *data) {
     (void)data;
 }
 
+static bool wait_response(t_data *data)
+{
+	struct timeval tv = {0, 100000};
+
+	fd_set readfds;
+	FD_ZERO(&readfds);
+	FD_SET(data->sockfd, &readfds);
+
+	int32_t retval = select(data->sockfd + 1, &readfds, NULL, NULL, &tv);
+	if (retval == -1)
+		error_exit_program(data, "select error");
+	else if (!retval)
+		return false;
+	return true;
+}
+
 void trace_pckt_route(t_data *data, struct sockaddr_in *addr_con) {
 
-	// socklen_t addr_len = sizeof(*addr_con);
 
     for (uint16_t ttl = 1; ttl <= 64; ttl++) {
 
-			t_icmp_pckt pckt;
-			char buffer[sizeof(struct iphdr) + sizeof(t_icmp_pckt)];
-			struct timespec time_start, time_end;
+		t_icmp_pckt pckt;
+		char buffer[sizeof(struct iphdr) + sizeof(t_icmp_pckt)];
+		struct timespec time_start, time_end;
 
-			init_icmp_pckt(&pckt, data, ttl);
-			memset(buffer, 0, sizeof(buffer));
-            clock_gettime(CLOCK_MONOTONIC, &time_start);
-            if (sendto(data->sockfd, &pckt, sizeof(pckt), 0, (struct sockaddr *)addr_con, sizeof(*addr_con)) <= 0)
-                error_exit_program(data, "sendto error");
+		init_icmp_pckt(&pckt, data, ttl);
+		memset(buffer, 0, sizeof(buffer));
+        clock_gettime(CLOCK_MONOTONIC, &time_start);
+        if (sendto(data->sockfd, &pckt, sizeof(pckt), 0, (struct sockaddr *)addr_con, sizeof(*addr_con)) <= 0)
+            error_exit_program(data, "sendto error");
 
-			struct sockaddr_in addr_;
-	        socklen_t addr_len = sizeof(addr_);
+			
 
-            if (recvfrom(data->sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&addr_, &addr_len) <= 0)
-                    error_exit_program(data, "recvfrom error");
-            
-            long double rtt_msec = get_ping_duration(&time_start, &time_end);
-            struct iphdr *ip_hdr = (struct iphdr *)buffer;
-            
-            printf("  %d    %s", ttl, inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
-        	printf("  %.3Lfms", rtt_msec);
-            printf("\n");	        
-    }
-    
+		struct sockaddr_in rcv_addr_con;
+	      socklen_t addr_len = sizeof(rcv_addr_con);
+
+		if (!wait_response(data)) {
+        	printf("%2d  *", ttl);
+		} else {
+			if (recvfrom(data->sockfd, buffer, sizeof(buffer), 0, (struct sockaddr *)&rcv_addr_con, &addr_len) <= 0)
+				error_exit_program(data, "recvfrom error");
+				
+			long double rtt_msec = get_ping_duration(&time_start, &time_end);
+
+			t_icmp_pckt rcvd_pckt;
+			struct iphdr *ip_hdr = (struct iphdr *)buffer;
+			struct icmphdr *icmp_hdr = (struct icmphdr *)(buffer + (ip_hdr->ihl * 4));
+
+			memcpy(&rcvd_pckt.hdr, icmp_hdr, sizeof(struct icmphdr));
+			memcpy(rcvd_pckt.payload, buffer + (ip_hdr->ihl * 4) + sizeof(struct icmphdr), PAYLOAD_SIZE);
+		
+			if (icmp_hdr->type == ICMP_TIME_EXCEEDED) {
+
+				printf("  %d    %s", ttl, inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
+				printf("  %.3Lfms", rtt_msec);
+				printf("\n");
+			} else if (icmp_hdr->type == ICMP_ECHOREPLY) {
+				printf("  %d    %s", ttl, inet_ntoa(*(struct in_addr *)&ip_hdr->saddr));
+				printf("  %.3Lfms", rtt_msec);
+				printf("\n");
+				return;
+			} else {
+				printf("	*");
+				printf("\n");
+			}	        
+    	}
+	}  
 }
 
 int main(int ac, char **av) {
